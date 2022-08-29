3b8754264b1378fe2e2414db60c14942fcd9b170
trelles821 <trelles821@gmail.com>
2022-08-29 13:49:50 -0400

    fixed bugs with structured_data fucntions; added store name retrieval

--- functions.py
+++ functions.py
@@ -82,6 +82,9 @@ def extract_data(text):
 
    
     def extract_prices(text):
+        if len(text) == 0:
+            return np.nan
+        
         prices = CommonRegex(text).prices
         if len(prices) > 0:
             return max(prices)
@@ -90,6 +93,9 @@ def extract_data(text):
 
         
     def extract_geo(text):
+        if len(text) == 0:
+            return np.nan, np.nan, np.nan, np.nan
+        
         #zip_code 
         us_zip = r'(\d{5}\-?\d{0,4})'
         zip_code = re.search(us_zip, text)
@@ -113,9 +119,18 @@ def extract_data(text):
         return zip_code, longitude, latitude, address
         
     def extract_time(text):
+        if len(text) == 0:
+            return np.nan
         times = CommonRegex(text).times
         if len(times) == 1:
             return times[0]
+        else:
+            return np.nan
+        
+    def extract_store_names(text):
+        string = text.strip()
+        if len(string) >0:
+            return re.search('[^(\n)]+', string, flags=0)[0]
         else:
             return np.nan
     
@@ -130,6 +145,8 @@ def extract_data(text):
     df['prices'] = [extract_prices(text)]
          
     df['time']= extract_time(text)
+    
+    df['Store Names'] = extract_store_names(text)
         
     return df
 
